ed3536be765c511742bd15c485467611d137b33b
alwaysnforever <ayaaitouamar@gmail.com>
2025-12-20 17:09:35 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include "wordle.h"
 
+#ifdef _WIN32
+#include <windows.h>
+void EnableANSIColors() {
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (hOut == INVALID_HANDLE_VALUE) return;
+
+    DWORD dwMode = 0;
+    if (!GetConsoleMode(hOut, &dwMode)) return;
+
+    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    SetConsoleMode(hOut, dwMode);
+}
+#endif
+
 int main() {
+
+    #ifdef _WIN32
+    EnableANSIColors();
+#endif
 
     srand(time(NULL));   // initialiser le générateur aléatoire
 
